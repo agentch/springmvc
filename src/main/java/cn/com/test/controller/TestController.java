22a0ccdc11713ca2b8a326c88ee9db894a8ce7fa
haley.git <gxbyl4321@163.com>
2016-07-17 21:30:19 +0800

    log

--- src/main/java/cn/com/test/controller/TestController.java
+++ src/main/java/cn/com/test/controller/TestController.java
@@ -1,6 +1,8 @@
 package cn.com.test.controller;
 
 import cn.com.test.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller("test")
 public class TestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private TestService testService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "test", method = RequestMethod.GET)
     @ResponseBody
     public String test() {
         return testService.test();
+    }
+
+    @RequestMapping(value = "test2", method = RequestMethod.GET)
+    @ResponseBody
+    public Integer test2(String s) {
+        try {
+            Integer i = Integer.parseInt(s);
+            return i;
+        } catch (Exception e) {
+            if (LOGGER.isErrorEnabled()) {
+                LOGGER.error(e.getMessage(), e);
+                return 0;
+            }
+        }
+        return -1;
     }
 
 }
